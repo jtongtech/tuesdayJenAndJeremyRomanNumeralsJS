f56dbda7265a21f6b150aba6f8494e82d87b7109
JTong <eldertong@gmail.com>
2017-10-03 16:33:37 -0400

    accounting for D

--- arabicNums.js
+++ arabicNums.js
@@ -2,11 +2,19 @@ console.assert(1 === 2, 'That is wrong bob!');
 var result = 0;
 var input_letter = "";
 
+function resetResult() {
+    if (result != 0) {
+        result = 0;
+    }
+}
+
 //Logic Section Below
 
 function romanToArabic(input_letter){
     if(input_letter == "M"){
         result += 1000;
+    }else if(input_letter == "D"){
+        result += 500;
     }
     return result;
 }
@@ -19,5 +27,7 @@ function testing(result, input_letter){
     console.assert(result == romanToArabic(input_letter), {"message":"result != to input_letter"});
 }
 testing(1000, romanToArabic("M"));
+resetResult()
 testing(500, romanToArabic("D"));
+resetResult()
 //Test Section Above
