e8fcb4c62bd1903969a8f8e642ee513005e193cd
JTong <eldertong@gmail.com>
2017-10-03 14:52:28 -0400

    accounting for individual roman numerals

--- romanNums.js
+++ romanNums.js
@@ -7,6 +7,14 @@ function changeNumber(input_number){
         result = "M";
     } else if (input_number == 500){
         result = "D";
+    } else if (input_number == 100){
+        result = "C";
+    } else if (input_number == 10){
+        result = "X";
+    } else if (input_number == 5){
+        result = "V";
+    } else if (input_number == 1){
+        result = "I";
     }
 }
 changeNumber(input_number);
@@ -40,5 +48,55 @@ function dEqualsFiveHundred() {
 }
 
 dEqualsFiveHundred();
+
+function cEqualsOneHundred() {
+    changeNumber(100);
+    console.log(result);
+    if (result == "C"){
+    console.log("cEqualsOneHundred passed");
+    }else{ 
+        (console.log("cEqualsOneHundred failed"));
+    }
+}
+
+cEqualsOneHundred();
+
+function xEqualsTen() {
+    changeNumber(10);
+    console.log(result);
+    if (result == "X"){
+    console.log("xEqualsTen passed");
+    }else{ 
+        (console.log("xEqualsTen failed"));
+    }
+}
+
+xEqualsTen();
+
+function vEqualsFive() {
+    changeNumber(5);
+    console.log(result);
+    if (result == "V"){
+    console.log("vEqualsFive passed");
+    }else{ 
+        (console.log("vEqualsFive failed"));
+    }
+}
+
+vEqualsFive();
+
+function iEqualsOne() {
+    changeNumber(1);
+    console.log(result);
+    if (result == "I"){
+    console.log("iEqualsOne passed");
+    }else{ 
+        (console.log("iEqualsOne failed"));
+    }
+}
+
+iEqualsOne();
+
+
 
 // Test Section Above
