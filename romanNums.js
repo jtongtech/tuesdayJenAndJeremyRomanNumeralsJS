28ff2459a3bd9eb5608443869cac90efbfb0595a
JTong <eldertong@gmail.com>
2017-10-03 14:24:01 -0400

    function mEqualsOneThousand()

---
+++ romanNums.js
@@ -0,0 +1,20 @@
+
+//Logic Section Below
+
+//Logic Sections Above
+
+// Test Section Below
+console.log("Hello World")
+
+var result = "";
+
+function mEqualsOneThousand() {
+    var input_number = "1000";
+    if (changeNumber(input_number) == "M")
+    console.log("mEqualsOneThousand passed");
+    else (console.log("mEqualsOneThousand failed"));
+}
+
+mEqualsOneThousand()
+
+// Test Section Above
